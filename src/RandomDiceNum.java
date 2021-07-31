27e53df8501c41c7093e86d8513ca29e7f724f15
Ehtasham <ehtashamhusain@gmail.com>
2021-07-31 23:10:31 +0530

    Average of 5 random 2 Digit Number

--- src/RandomDiceNum.java
+++ src/RandomDiceNum.java
@@ -2,11 +2,12 @@ public class RandomDiceNum {
 
     public static void main(String[] args) {
         int temp = 0;
-            for (int i = 1;  i<=2; i++){
-            int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            for (int i = 1;  i<=5; i++){
+            int dice = (int) Math.floor(Math.random() * 100) % 90 + 10;
                 System.out.println("Dice"+i+" = "+dice);
             temp += dice;
         }
-        System.out.println("Addition of Two Random Dice = "+temp);
+        System.out.println("Addition of five Random Dice = "+temp);
+        System.out.println("Average of 5 random dice number = "+temp/5);
     }
 }
