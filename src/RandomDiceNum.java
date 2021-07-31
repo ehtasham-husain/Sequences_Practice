4340602f891b22b79f26a9a5502a7632003ed1fd
Ehtasham <ehtashamhusain@gmail.com>
2021-07-31 14:33:26 +0530

    Updated

--- src/RandomDiceNum.java
+++ src/RandomDiceNum.java
@@ -2,7 +2,7 @@ public class RandomDiceNum {
 
     public static void main(String[] args) {
         int i=0;
-        while (i<20){
+        while (i < 20){
             int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
             System.out.println(dice);
             i++;
