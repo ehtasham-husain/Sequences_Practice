2cb7857b1cfbf4a68bf5fc7590d1388e983fa4f5
Ehtasham <ehtashamhusain@gmail.com>
2021-07-31 14:30:08 +0530

    Use Random to get Dice Number between 1 to 6

---
+++ src/RandomDiceNum.java
@@ -0,0 +1,11 @@
+public class RandomDiceNum {
+
+    public static void main(String[] args) {
+        int i=0;
+        while (i<20){
+            int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            System.out.println(dice);
+            i++;
+        }
+    }
+}
