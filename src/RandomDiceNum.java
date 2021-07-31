fa32bbdabc7696b7be9a34ca0c6c4b4c74e5d9f3
Ehtasham <ehtashamhusain@gmail.com>
2021-07-31 22:44:47 +0530

    Add 2 Random Dice Number

--- src/RandomDiceNum.java
+++ src/RandomDiceNum.java
@@ -1,11 +1,12 @@
 public class RandomDiceNum {
 
     public static void main(String[] args) {
-        int i=0;
-        while (i < 20){
+        int temp = 0;
+            for (int i = 1;  i<=2; i++){
             int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-            System.out.println(dice);
-            i++;
+                System.out.println("Dice"+i+" = "+dice);
+            temp += dice;
         }
+        System.out.println("Addition of Two Random Dice = "+temp);
     }
 }
